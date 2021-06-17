31ac4799571628aa8eb05165e4f767cd57c96c95
Takumi Crary <tcrary@gmail.com>
2021-06-17 07:12:12 +0100

    Add some matrix math stuff for affine transformations

--- sdl-helloworld/app/Main.hs
+++ sdl-helloworld/app/Main.hs
@@ -9,17 +9,48 @@ import SDL.Vect
 import qualified SDL
 import qualified SDL.Event as SDL
 
+import Data.Bifunctor
+import Data.Word
 import SDL.Image
 import Linear
-
+import SDL.Primitive
 
 title = "My SDL Application"
 screenWidth, screenHeight :: CInt
 (screenWidth, screenHeight) = (640, 480)
 
-vertical_lines = [((V2 x 0), (V2 x 64)) | x <- [0..64]]   :: [(V2 CInt, V2 CInt)]
-horizontal_lines = [((V2 0 y), (V2 64 y)) | y <- [0..64]] :: [(V2 CInt, V2 CInt)]
+type Line = (V3 CInt, V3 CInt)
+vertical_lines = [(homo (V2 x 0), homo (V2 x 64)) | x <- [0..10]]
+horizontal_lines = [(homo (V2 0 y), homo (V2 64 y)) | y <- [0..10]]
+base_lines = vertical_lines ++ horizontal_lines
 
+zoom_matrix :: (Num a) => a -> V3 (V3 a)
+zoom_matrix scale = V3 (V3 scale 0     0)
+                       (V3 0     scale 0)
+                       (V3 0     0     1)
+
+translate_matrix :: (Num a) => a -> a -> V3 (V3 a)
+translate_matrix x y = V3 (V3 0 0 x)
+                          (V3 0 0 y)
+                          (V3 0 0 1)
+
+rotation_matrix :: Double -> V3 (V3 Double)
+rotation_matrix theta = V3 (V3 (cos theta) (-sin theta) 0)
+                           (V3 (sin theta) (cos theta)  0)
+                           (V3 0 0 1)
+
+rotate_around :: Double -> (V2 Double) -> (V3 (V3 Double))
+rotate_around theta (V2 x y) = (translate_matrix x y) !*! (rotation_matrix theta) !*! (translate_matrix (-x) (-y))
+
+idv3 = V3 (V3 1 0 0) (V3 0 1 0) (V3 0 0 1) :: V3 (V3 CInt)
+apply_transform :: V3 (V3 CInt) -> [Line] -> [Line]
+apply_transform t xs = fmap (bimap (*! t) (*! t)) xs
+
+homo :: (Num a) => V2 a -> V3 a
+homo (V2 x y) = V3 x y 1
+
+dropHomo :: (Num a) => V3 a -> V2 a
+dropHomo (V3 x y _) = V2 x y
 
 main :: IO ()
 main = do
@@ -32,14 +63,22 @@ main = do
 
     screenSurface <- SDL.getWindowSurface window
     let white = SDL.V4 maxBound maxBound maxBound maxBound
-    SDL.surfaceFillRect screenSurface Nothing white
+    let black = SDL.V4 0 0 0 0
+    SDL.surfaceFillRect screenSurface Nothing black
     SDL.updateWindowSurface window
+
+    screenRenderer <- SDL.createSoftwareRenderer screenSurface
 
     let loop = do
             events <- SDL.pollEvents :: IO [SDL.Event]
             let quit = elem SDL.QuitEvent $ map SDL.eventPayload events
         --Do stuff here, we can pass a monad in or somen
-            SDL.surfaceBlit garg Nothing screenSurface Nothing
+          --SDL.surfaceBlit garg Nothing screenSurface Nothing
+
+            let lines = apply_transform (idv3) vertical_lines
+            forM_ (apply_transform (zoom_matrix 10) vertical_lines) (\(start, end) -> line screenRenderer (dropHomo start) (dropHomo end) white)
+
+            --forM_ horizontal_lines (\(start,end) -> line screenRenderer start end white)
             SDL.updateWindowSurface window
 
             unless quit loop
@@ -47,5 +86,6 @@ main = do
 
     loop
 
+    SDL.freeSurface screenSurface
     SDL.destroyWindow window
     SDL.quit
