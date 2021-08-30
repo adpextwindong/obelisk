52c9f3b945afeda38ecc48348eba65965f991f74
Takumi Crary <tcrary@gmail.com>
2021-08-30 04:58:24 +0100

    Throw more shit at the wall for intersections

--- src/Obelisk/Engine/DDA2.hs
+++ src/Obelisk/Engine/DDA2.hs
@@ -9,6 +9,7 @@ import qualified Data.Set as S
 import Obelisk.Graphics.Primitives
 import Obelisk.Graphics.DebugUI
 import Obelisk.State (emptyMap)
+import SDL.Primitive (horizontalLine)
 
 --AmanatidesWoo Algorithmn
 --We need to get actual intersection positions
@@ -66,27 +67,57 @@ naivePath ws player direction = mergeIntersections player vints hints
 -- All x's will end up at whole numbers on the rayline
 -- TODO make a test to make sure these lay on the line
 -- v ray
-verticalIntersections :: V2 Double -> V2 Double -> [V2 Double]
-verticalIntersections p@(V2 px _) v@(V2 vx _) = vert_rayPoints
-    where
-        eInds = evalInds px vx
-        vert_rayPoints = fmap ((p +). (v ^*)) eInds
+-- verticalIntersections :: V2 Double -> V2 Double -> [V2 Double]
+-- verticalIntersections p@(V2 px _) v@(V2 vx _) = vert_rayPoints
+--     where
+--         eInds = evalInds px vx
+--         vert_rayPoints = fmap ((p +). (v ^*)) eInds
+verticalIntersections p r = xForm p r
+horizontalIntersections p r = yForm p r
 
 -- All y's will end up at whole numbers on the rayline
 -- v is the ray
-horizontalIntersections :: V2 Double -> V2 Double -> [V2 Double]
-horizontalIntersections p@(V2 px py) v@(V2 _ vy) = horz_rayPoints --TODO
-    where
-        eInds = evalInds py vy
-        horz_rayPoints = fmap ((p +). ((swapScale v) ^*)) eInds
+-- horizontalIntersections :: V2 Double -> V2 Double -> [V2 Double]
+-- horizontalIntersections p@(V2 px py) v@(V2 _ vy) = horz_rayPoints --TODO
+--     where
+--         eInds = evalInds py vy
+--         horz_rayPoints = fmap ((p +). ((swapScale v) ^*)) eInds
 
+--SHIT
+-- firstIntersections :: V2 Double -> V2 Double -> ([V2 Double],[V2 Double])
+-- firstIntersections p@(V2 px py) r@(V2 rx ry) = (xIntersections yInterBase r 0, yIntersections xInterBase r 0)
+--     where 
+--         (xFirst, yFirst) = deltaFirsts p r
+--         yInterBase = V2 (xFirst + px) (py + (xFirst*ry))
+        
+--         xInterBase = V2 (px + (yFirst * rx)) (yFirst + py)
+
+-- xIntersections yB r df = [yB + xStep r i  | i <- [df, df+1.0..]]
+-- yIntersections xB r df = [xB + yStep r i | i <- [df, df+1.0..]]
+-- deltaFirsts p r = (deltaFirst (p^._x) (r^._x), deltaFirst (p^._y) (r^._y))
+-- yStep r t = t *^ r ^* (1/ abs (r^._y))
+-- xStep r t = t *^ r ^* (1/ abs (r^._x))
+
+--TODO test these forms with rays in all directions
+xForm p r = [((i - p^._x)/(r^._x)) *^ r + p | i <- fmap (signToPlusNegOne (r^._x) *) [1.00..10.0]]
+yForm p r = [((i - p^._y)/(r^._y)) *^ r + p | i <- fmap (signToPlusNegOne (r^._y) *) [1.00..10.0]]
+
+
+
+-- verticalPoints :: V2 Double -> V2 Double -> [V2 Double]
+
+deltaFirst :: Double -> Double -> Double 
+deltaFirst px vx = if vx < 0
+                   then fromIntegral (floor px) - px
+                   else fromIntegral (ceiling px) - px
+--TODO make sure this works with rays in all directions
 absDeltaFirst :: Double -> Double -> Double
 absDeltaFirst px vx = if vx < 0
-                   then px - (fromIntegral (floor px))
-                   else (fromIntegral (ceiling px)) - px
+                      then px - (fromIntegral (floor px))
+                      else (fromIntegral (ceiling px)) - px
 
 --Eval the ray at these points and add player position to get the intersection points
-evalInds px vx = (\absf -> [absf, (absf + 1.0) ..]) (absDeltaFirst px vx)
+evalInds px vx = (\b -> [b, (b + 1.0) ..]) (deltaFirst px vx)
 
 
 naiveBumpPath :: Int -> V2 Double -> V2 Double -> [(V2 Double, V2 Int)]
@@ -109,20 +140,26 @@ mergeBumpIntersections player ray (x:xs) (y:ys) = if distance player x < distanc
 mergeBumpIntersections _ ray [] ys = (\y -> (y, yBumpFloorRay ray y)) <$> ys
 mergeBumpIntersections _ ray xs [] = (\x -> (x, xBumpFloorRay ray x)) <$> xs
 --is xybump needed?
+
+xBumpFloorRay :: V2 Double -> V2 Double -> V2 Int
 xBumpFloorRay (V2 vx _) inter@(V2 x y) = if vx < 0
                                          then fmap round inter--V2 ((floor x) - 1) (floor y)
                                          else V2 (round x) (truncate y)
 
+yBumpFloorRay :: V2 Double -> V2 Double -> V2 Int
 yBumpFloorRay (V2 _ vy) inter@(V2 x y) = if vy < 0
                                          then fmap round inter --V2 (floor x) ((floor y) - 1)
                                          else V2 (truncate x) (round y)  --TODO make sure this is right
 --TODO check if this produces the correct traversal
 
+swapScale :: Fractional a => V2 a -> V2 a
 swapScale v@(V2 _ y) = (1.0/y) *^ v
 -- tgp =  GroupPrim "" $ (\c -> Prim (Circle c 1 (V4 255 255 255 255)))
             -- <$> [V2 x y |  x <- [0..10], y <- [0..10]]
 
-clipWorld ws = takeWhile (\v@(V2 x y) -> x <= (fromIntegral ws) && y <= (fromIntegral ws))
+clipWorld ws = takeWhile (\v@(V2 x y) -> x <= fromIntegral ws && y <= fromIntegral ws
+                                            && x >= 0 && y >= 0)
+
 p = V2 0.25 0.66
 -- p = V2 0 0
 r = V2 1 2
