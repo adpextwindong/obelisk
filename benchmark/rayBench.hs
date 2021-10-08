f107a5865af5de62fde2f7a658223f8a129ea7a3
Takumi Crary <tcrary@gmail.com>
2021-10-08 05:23:29 +0100

    Switch benchmark to normal form to see times

--- benchmark/rayBench.hs
+++ benchmark/rayBench.hs
@@ -14,7 +14,14 @@ p = PVars (V2 2.5 6.5) dir cam
 main :: IO ()
 main = defaultMain [
     bgroup "genRays" [
-        bench "r640" $ whnf (genRays 640 p) 10,
-        bench "r320" $ whnf (genRays 320 p) 10,
-        bench "r240" $ whnf (genRays 240 p) 10]
+        bench "r1920 w10" $ nf (genRays 1920 p) 10,
+        bench "r1024 w10" $ nf (genRays 1024 p) 10,
+        bench "r640 w10" $ nf (genRays 640 p) 10,
+        bench "r320 w10" $ nf (genRays 320 p) 10,
+        bench "r240 w10" $ nf (genRays 240 p) 10,
+        bench "r1920 w64" $ nf (genRays 1920 p) 64,
+        bench "r1024 w64" $ nf (genRays 1024 p) 64,
+        bench "r640 w64" $ nf (genRays 640 p) 64,
+        bench "r320 w64" $ nf (genRays 320 p) 64,
+        bench "r240 w64" $ nf (genRays 240 p) 64]
         ]
