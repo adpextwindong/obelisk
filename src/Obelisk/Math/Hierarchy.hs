223f5a31accead257cd0cc7997aba0bf6ab19d43
Takumi Crary <tcrary@gmail.com>
2021-11-10 02:33:32 +0000

    Refactor the coordinate hierarchy idea into a workable state that doesn't use datakinds too heavily

--- src/Obelisk/Math/Hierarchy.hs
+++ src/Obelisk/Math/Hierarchy.hs
@@ -1,22 +1,47 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ConstraintKinds #-}
 module Obelisk.Math.Hierarchy where
-
 import Obelisk.Math.Homogenous
 
-data M22CoordAffine t (a :: * -> *) (b :: * -> *) where
+-- | a and b represent the source coordinate system and target coordinate system the Affine transformation takes you through.
+data M22CoordAffine t a b where
     M22CoordAffine :: M22Affine t -> M22CoordAffine t a b
 
-data CoordinateSystem t = 'WorldC t | 'NormalizedDevicedC t | 'PhysicalDeviceC t
+data WorldC
+data NDC
+data PDC
 
-testM22 :: M22Affine Float
-testM22 = undefined
+data CoordinateSystem = 'CoordinateSystem
 
---Yeah this shit sucks, lets try Proxy instead. I have no clue how to instantiate a type signature that fits.
--- test = M22CoordAffine testM22 :: M22CoordAffine Float ('WorldC Float) ('PhysicalDeviceC)
+type family KCoordinateSystem' a where
+    KCoordinateSystem' WorldC = 'CoordinateSystem
+    KCoordinateSystem' NDC = 'CoordinateSystem
+    KCoordinateSystem' PDC = 'CoordinateSystem
 
+type KCoordinateSystem t = (KCoordinateSystem' t ~ 'CoordinateSystem)
+
+-- | t is the underlying type for the V2/V3's to accomodate converting from Float to CInt eventually
+-- | a is the coordinate system (enforced by Lift's ConstraintKind usage)
 data HGraphic t a where
-    ApTransform :: HGraphic t (a t -> b t) -> HGraphic t (a t) -> HGraphic t (b t)
-    FunnyAffineT :: M22CoordAffine t a b -> HGraphic t (a t -> b t)
+    LiftHT :: (KCoordinateSystem a, KCoordinateSystem b) => M22CoordAffine t a b -> HGraphic t (a -> b)
+    ApTransform :: HGraphic t (a -> b) -> HGraphic t a -> HGraphic t b
+    Compose :: HGraphic t (a -> b) -> HGraphic t (b -> c) -> HGraphic t (a -> c)
+
+foo :: M22CoordAffine Float WorldC NDC
+foo = M22CoordAffine m22AffineIdD :: M22CoordAffine Float WorldC NDC
+
+bar :: M22CoordAffine Float NDC PDC
+bar = M22CoordAffine m22AffineIdD :: M22CoordAffine Float NDC PDC
+
+worldToPhys :: HGraphic Float (WorldC -> PDC)
+worldToPhys = Compose (LiftHT foo) (LiftHT bar)
+
+{- Does not type check
+quux = Compose (LiftHT bar) (LiftHT foo)
+
+• Couldn't match type ‘WorldC’ with ‘PDC’
+  Expected type: HGraphic Float (PDC -> NDC)
+    Actual type: HGraphic Float (WorldC -> NDC)
+-}
